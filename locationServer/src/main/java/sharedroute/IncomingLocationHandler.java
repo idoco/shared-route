3a01d0dec5975822f5d0585539c2c3b33df009f4
idoco <ido.c.mail@gmail.com>
2015-03-13 18:50:18 +0200

    Server refactor. SharedData sets replaced with event pub-sub pattern. The LocationVerticle should now support multiple verticals as a cluster. From now on the sender of a location update will also receive. It is up to the client to filter these echoing updates if it chooses to.

--- locationServer/src/main/java/sharedroute/IncomingLocationHandler.java
+++ locationServer/src/main/java/sharedroute/IncomingLocationHandler.java
@@ -11,7 +11,6 @@ import org.vertx.java.platform.Container;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Set;
 
 /**
 * Created by cohid01 on 08/03/2015.
@@ -24,39 +23,32 @@ class IncomingLocationHandler implements Handler<Buffer> {
     private final Vertx vertx;
     private final Logger _log;
     private final List<LatLng> route;
-    private final String wsHandlerId;
 
-    public IncomingLocationHandler(Container container, Vertx vertx, String wsHandlerId, List<LatLng> route) {
+    public IncomingLocationHandler(Container container, Vertx vertx, List<LatLng> route) {
         this._log = container.logger();
         this.vertx = vertx;
-        this.wsHandlerId = wsHandlerId;
         this.route = route;
     }
 
     public void handle(Buffer data) {
+        String incomingData = data.toString();
         try {
-            _log.debug("Incoming location message [" + data.toString() + "]");
+            _log.debug("Incoming location message [" + incomingData + "]");
             JsonNode rootNode = mapper.readTree(data.getBytes());
-            // String sessionId = rootNode.get("sessionId").asText();
+            String sessionId = rootNode.get("sessionId").asText();
             double lat = rootNode.get("lat").asDouble();
             double lng = rootNode.get("lng").asDouble();
             LatLng newPoint = new LatLng(lat, lng);
 
             // Consider braking this into different handlers
             if (pointIsOnRoute(newPoint)) {
-                Set<String> connections = vertx.sharedData().getSet(LocationVerticle.CONNECTION_SET);
-                for (String registeredWsHandlerId : connections) {
-                    // do not send incoming message to its sender
-                    if (!wsHandlerId.equals(registeredWsHandlerId)) {
-                        vertx.eventBus().send(registeredWsHandlerId, data);
-                    }
-                }
+                vertx.eventBus().publish(LocationVerticle.LOCATION_PUBLISHED, incomingData);
             } else {
-                _log.warn("Filtering out location from connection ["+wsHandlerId+"] location is ["+newPoint+"]");
+                _log.warn("Filtering out location from connection [" + sessionId + "] location is [" + newPoint + "]");
             }
 
         } catch (IOException e) {
-            _log.error("Error parsing json [" + data.toString() + "]");
+            _log.error("Error parsing json [" + incomingData + "]");
         }
     }
 
--- locationServer/src/main/java/sharedroute/LocationVerticle.java
+++ locationServer/src/main/java/sharedroute/LocationVerticle.java
@@ -2,45 +2,52 @@ package sharedroute;
 
 import com.javadocmd.simplelatlng.LatLng;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.ServerWebSocket;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/*
-A simple Java verticle which receives user location messages and publish them to all other users.
- */
+/**
+ * A simple Java verticle which receives user location messages and publish them to all other users.
+ **/
 public class LocationVerticle extends Verticle {
 
-    public static final String CONNECTION_SET = "sharedroute.connections";
+    public static final String LOCATION_PUBLISHED = "location.published";
+    public Set<ServerWebSocket> connectedClients = new HashSet<>();
 
     public void start() {
         final Logger _log = container.logger();
         final Pattern chatUrlPattern = Pattern.compile("/app");
         final List<LatLng> route = MapUtils.parseRouteFromGeoJson(vertx, "map.geojson");
-        _log.info("Websocket verticle up");
+        _log.info("Websocket verticle is up");
 
         vertx.createHttpServer().websocketHandler(new Handler<ServerWebSocket>() {
             public void handle(final ServerWebSocket ws) {
                 final Matcher matcher = chatUrlPattern.matcher(ws.path());
                 if (matcher.matches()) {
-                    final String wsHandlerId = ws.binaryHandlerID();
-                    registerConnection(wsHandlerId);
-                    ws.dataHandler(new IncomingLocationHandler(container, vertx, wsHandlerId, route));
+                    connectedClients.add(ws);
+                    _log.info("Connection registered. Current number of connections: " + connectedClients.size());
+                    ws.dataHandler(new IncomingLocationHandler(container, vertx, route));
+
                     ws.closeHandler(new Handler<Void>() {
                         @Override
                         public void handle(Void event) {
-                            unregisterConnection(wsHandlerId);
+                            connectedClients.remove(ws);
+                            _log.info("Connection closed. Current number of connections: " + connectedClients.size());
                         }
                     });
+
                     ws.exceptionHandler(new Handler<Throwable>() {
                         @Override
                         public void handle(Throwable event) {
-                            _log.error("Connection error", event);
-                            unregisterConnection(wsHandlerId);
+                            connectedClients.remove(ws);
+                            _log.error("Connection error. Current number of connections: " + connectedClients.size());
                         }
                     });
 
@@ -49,23 +56,18 @@ public class LocationVerticle extends Verticle {
                     ws.reject();
                 }
             }
-
-            private void registerConnection(String wsHandlerId) {
-                vertx.sharedData().getSet(CONNECTION_SET).add(wsHandlerId);
-                _log.info("Connection registered [id: " + wsHandlerId + "] " +
-                        "current number of connections: " + getNumberOfConnections());
-            }
-
-            private void unregisterConnection(String wsHandlerId) {
-                vertx.sharedData().getSet(CONNECTION_SET).remove(wsHandlerId);
-                _log.info("Connection closed [id: " + wsHandlerId + "] " +
-                        "current number of connections: " + getNumberOfConnections());
-            }
-
-            private int getNumberOfConnections() {
-                return vertx.sharedData().getSet(CONNECTION_SET).size();
-            }
-
         }).listen(8080);
+
+        // Publish new location updates to all connected clients
+        vertx.eventBus().registerHandler(LOCATION_PUBLISHED,new Handler<Message<String>>() {
+            @Override
+            public void handle(Message<String> event) {
+                String locationUpdateJson = event.body();
+                for (ServerWebSocket serverWebSocket : connectedClients) {
+                    serverWebSocket.writeTextFrame(locationUpdateJson);
+                }
+            }
+        });
+
     }
 }
