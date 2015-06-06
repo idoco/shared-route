e4af032b69a79db1c35740af358c32ea5a204e83
“idoco” <ido.c.mail@gmail.com>
2015-06-06 20:26:51 +0300

    Fixed modernizr check.

--- javascripts/main.js
+++ javascripts/main.js
@@ -157,12 +157,13 @@ function toggleSharingMode(){
     }
 }
 
-Materialize.toast('websocket support: '+Modernizr.websockets, 10000);
-Materialize.toast('geolocation support: '+Modernizr.geolocation, 10000);
-
 google.maps.event.addDomListener(window, 'load', initialize);
 
 $( document ).ready(function() {
+    if(!Modernizr.websockets || !Modernizr.geolocation){
+        Materialize.toast('Browser not supported :(', 10000);
+    }
+
     $(".button-collapse").sideNav();
     shareRideButton = $("#share-ride-button")[0];
 });
